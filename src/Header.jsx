5038f70371309a18bb67a2b06c0378391d246f2d
rimpei0121 <sadachbia0121@gmail.com>
2025-05-22 17:35:37 +0900

    ヘッダーメニューを実装

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,19 +5,27 @@ const Header = () => {
   return (
     <div className="header">
       <img src="src/assets/img/Logo.png" alt="" className='header-logo' />
-
-      {/* <div className="humberger">
-        <span className="bar"></span><span className="bar"></span><span className="bar"></span>
-      </div> */}
-
       <div className="hamburger" onClick={ (el) =>{
         el.target.classList.toggle("open")
         console.log(el.target.classList)
+        const menuDOM = document.querySelector('.menu-list')
+        menuDOM.classList.toggle('open')
+        const headerDOM = document.querySelector('.header')
+        headerDOM.classList.toggle('open')
       } } >
         <span></span>
         <span></span>
         <span></span>
       </div>
+
+      <ul className="menu-list">
+        <li className="menu-item">
+          <a href="/">HOME</a>
+        </li>
+        <li className="menu-item">
+          <a href="/yahaha">Yahaha</a>
+        </li>
+      </ul>
     </div>
   )
 }
